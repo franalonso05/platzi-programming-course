c1f031db4f1363d84cd4fea33b050f0a0c552758
Francisco Alonso <fbartolomealonso@gmail.com>
2023-11-12 16:05:10 -0300

    Class-20 Class-21: Manipulated de DOM to choose the player's pet by using "innerHTML"

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,7 +4,20 @@ function startGame() {
 }
 
 function selectPet(){
-    alert("YOU SELECTED YOUR PET")
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipego = document.getElementById('capipego')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanPlayerPet = document.getElementById('player-pet')
+
+    if (inputHipodoge.checked) {
+        spanPlayerPet.innerHTML = 'Hipodoge'
+    } else if (inputCapipego.checked) {
+        spanPlayerPet.innerHTML = 'Capipego'
+    } else if (inputRatigueya.checked) {
+        spanPlayerPet.innerHTML = 'Ratigueya'
+    } else {
+        alert("Select a pet")
+    }
 }
 
 window.addEventListener('load', startGame)
