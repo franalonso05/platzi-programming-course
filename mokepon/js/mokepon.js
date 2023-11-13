5ab7995f48a712c69df8e85f9e6c7fb1ede921fa
Francisco Alonso <fbartolomealonso@gmail.com>
2023-11-13 09:44:22 -0300

    Class-22 Class-23: Declared the function to choose a random enemy's pet, also given a function to each attack button

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,17 @@
+let playerAttack
+
 function startGame() {
     let buttonPet = document.getElementById('button-pet')
     buttonPet.addEventListener('click', selectPet)
+
+    let buttonFire = document.getElementById('button-fire')
+    buttonFire.addEventListener('click', fireAttack)
+    let buttonWater = document.getElementById('button-water')
+    buttonWater.addEventListener('click', waterAttack)
+    let buttonLand = document.getElementById('button-land')
+    buttonLand.addEventListener('click', landAttack)
+
+
 }
 
 function selectPet(){
@@ -18,6 +29,39 @@ function selectPet(){
     } else {
         alert("Select a pet")
     }
+
+    selectEnemyPet()
 }
+
+function selectEnemyPet() {
+    let randomattack = aleatorio(1,3)
+    let spanEnemyPet = document.getElementById('enemys-pet')
+
+    if (randomattack == 1) {
+        spanEnemyPet.innerHTML = 'Hipodoge'
+    } else if (randomattack == 2) {
+        spanEnemyPet.innerHTML = 'Capipego'
+    } else {
+        spanEnemyPet.innerHTML = 'Ratigueya'
+    }
+}
+
+function fireAttack() {
+    playerAttack = "FIRE"
+    alert(playerAttack)
+}
+function waterAttack() {
+    playerAttack = "WATER"
+    alert(playerAttack)
+}
+function landAttack() {
+    playerAttack = "LAND"
+    alert(playerAttack)
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 
 window.addEventListener('load', startGame)
