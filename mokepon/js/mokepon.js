1de456ae218a8420bd62169bfcfac00379f2bf3e
Francisco Alonso <fbartolomealonso@gmail.com>
2023-11-14 09:26:55 -0300

    Class-29 Class-31: Added the function to reset the game, also hid HTML elements with JS to improve the game's UX

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,8 +4,16 @@ let playerLives = 3
 let enemyLives = 3
 
 function startGame() {
+    let sectionSelectAttack = document.getElementById('select-attack')
+    sectionSelectAttack.style.display = 'none'
+    
+    let sectionResetGame = document.getElementById('reset')
+    sectionResetGame.style.display = 'none'
+
     let buttonPet = document.getElementById('button-pet')
     buttonPet.addEventListener('click', selectPet)
+
+    
 
     let buttonFire = document.getElementById('button-fire')
     buttonFire.addEventListener('click', fireAttack)
@@ -14,10 +22,17 @@ function startGame() {
     let buttonLand = document.getElementById('button-land')
     buttonLand.addEventListener('click', landAttack)
 
-
+    let resetButton = document.getElementById('button-reset')
+    resetButton.addEventListener('click', resetGame)
 }
 
 function selectPet(){
+    let sectionSelectAttack = document.getElementById('select-attack')
+    sectionSelectAttack.style.display = 'block'
+
+    let sectionSelectPet = document.getElementById('select-pet')
+    sectionSelectPet.style.display = 'none'
+
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipego = document.getElementById('capipego')
     let inputRatigueya = document.getElementById('ratigueya')
@@ -92,6 +107,16 @@ function combat() {
         spanPlayerLives.innerHTML = playerLives
 
     }
+
+    reviewLives()
+}
+
+function reviewLives() {
+    if(enemyLives == 0) {
+        createFinalMessage("CONGRATULATIONS! YOU WON :)")
+    } else if(playerLives == 0) {
+        createFinalMessage("SORRY, YOU LOST :(")
+    }
 }
 
 function createMessage(result) {
@@ -101,6 +126,29 @@ function createMessage(result) {
     paragraph.innerHTML = 'Your pet attacked with ' + playerAttack + ', the enemys pet attacked with ' + enemysAttack + '- ' + result
 
     sectionMessages.appendChild(paragraph)
+}
+
+function createFinalMessage(finalResult) {
+    let sectionMessages = document.getElementById('messages')
+
+    let paragraph = document.createElement('p')
+    paragraph.innerHTML = finalResult
+
+    sectionMessages.appendChild(paragraph)
+
+    let buttonFire = document.getElementById('button-fire')
+    buttonFire.disabled = true
+    let buttonWater = document.getElementById('button-water')
+    buttonWater.disabled = true
+    let buttonLand = document.getElementById('button-land')
+    buttonLand.disabled = true
+
+    let sectionResetGame = document.getElementById('reset')
+    sectionResetGame.style.display = 'block'
+}
+
+function resetGame() {
+    location.reload()
 }
 
 function aleatorio(min, max) {
