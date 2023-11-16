e8e6346ea12ac96017b30aec0fc5854bfef804e4
Francisco Alonso <fbartolomealonso@gmail.com>
2023-11-16 10:38:36 -0300

    Class-44 Class-45: Revised the code by implementing DRY

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,42 +1,51 @@
+const sectionSelectAttack = document.getElementById('select-attack')
+const sectionResetGame = document.getElementById('reset')
+const buttonPet = document.getElementById('button-pet')
+const buttonFire = document.getElementById('button-fire')
+const buttonWater = document.getElementById('button-water')
+const buttonLand = document.getElementById('button-land')
+const resetButton = document.getElementById('button-reset')
+
+const sectionSelectPet = document.getElementById('select-pet')
+const inputHipodoge = document.getElementById('hipodoge')
+const inputCapipego = document.getElementById('capipego')
+const inputRatigueya = document.getElementById('ratigueya')
+const spanPlayerPet = document.getElementById('player-pet')
+
+const spanEnemyPet = document.getElementById('enemys-pet')
+
+const spanPlayerLives = document.getElementById('player-lives')
+const spanEnemyLives = document.getElementById('enemy-lives')
+
+const sectionMessages = document.getElementById('result')
+const playerAttacks = document.getElementById('player-attacks')
+const enemyAttacks = document.getElementById('enemy-attacks')
+
 let playerAttack
 let enemysAttack
 let playerLives = 3
 let enemyLives = 3
 
 function startGame() {
-    let sectionSelectAttack = document.getElementById('select-attack')
-    sectionSelectAttack.style.display = 'none'
     
-    let sectionResetGame = document.getElementById('reset')
+    sectionSelectAttack.style.display = 'none'
     sectionResetGame.style.display = 'none'
 
-    let buttonPet = document.getElementById('button-pet')
     buttonPet.addEventListener('click', selectPet)
 
-    
-
-    let buttonFire = document.getElementById('button-fire')
     buttonFire.addEventListener('click', fireAttack)
-    let buttonWater = document.getElementById('button-water')
+    
     buttonWater.addEventListener('click', waterAttack)
-    let buttonLand = document.getElementById('button-land')
+    
     buttonLand.addEventListener('click', landAttack)
 
-    let resetButton = document.getElementById('button-reset')
     resetButton.addEventListener('click', resetGame)
 }
 
 function selectPet(){
-    let sectionSelectAttack = document.getElementById('select-attack')
+    
     sectionSelectAttack.style.display = 'flex'
-
-    let sectionSelectPet = document.getElementById('select-pet')
     sectionSelectPet.style.display = 'none'
-
-    let inputHipodoge = document.getElementById('hipodoge')
-    let inputCapipego = document.getElementById('capipego')
-    let inputRatigueya = document.getElementById('ratigueya')
-    let spanPlayerPet = document.getElementById('player-pet')
 
     if (inputHipodoge.checked) {
         spanPlayerPet.innerHTML = 'Hipodoge'
@@ -53,8 +62,7 @@ function selectPet(){
 
 function selectEnemyPet() {
     let randompet = aleatorio(1,3)
-    let spanEnemyPet = document.getElementById('enemys-pet')
-
+    
     if (randompet == 1) {
         spanEnemyPet.innerHTML = 'Hipodoge'
     } else if (randompet == 2) {
@@ -92,9 +100,7 @@ function randomEnemysAttack() {
 }
 
 function combat() {
-    let spanPlayerLives = document.getElementById('player-lives')
-    let spanEnemyLives = document.getElementById('enemy-lives')
-
+    
     if(enemysAttack == playerAttack){
         createMessage("TIE")
     } else if((playerAttack == 'FIRE' && enemysAttack == 'LAND') || (playerAttack == 'WATER' && enemysAttack == 'FIRE') || (playerAttack == 'LAND' && enemysAttack == 'WATER')) {
@@ -105,7 +111,6 @@ function combat() {
         createMessage("YOU LOSE")
         playerLives--
         spanPlayerLives.innerHTML = playerLives
-
     }
 
     reviewLives()
@@ -120,10 +125,6 @@ function reviewLives() {
 }
 
 function createMessage(result) {
-    let sectionMessages = document.getElementById('result')
-    let playerAttacks = document.getElementById('player-attacks')
-    let enemyAttacks = document.getElementById('enemy-attacks')
-
     
     let newPlayerAttack = document.createElement('p')
     let newEnemyAttack = document.createElement('p')
@@ -137,18 +138,13 @@ function createMessage(result) {
 }
 
 function createFinalMessage(finalResult) {
-    let sectionMessages = document.getElementById('result')
-
+    
     sectionMessages.innerHTML = finalResult
 
-    let buttonFire = document.getElementById('button-fire')
     buttonFire.disabled = true
-    let buttonWater = document.getElementById('button-water')
     buttonWater.disabled = true
-    let buttonLand = document.getElementById('button-land')
     buttonLand.disabled = true
 
-    let sectionResetGame = document.getElementById('reset')
     sectionResetGame.style.display = 'block'
 }
 
